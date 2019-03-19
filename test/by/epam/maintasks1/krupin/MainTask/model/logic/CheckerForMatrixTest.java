923e445ea53c87a70047f6520bd7f0d337508090
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-03-20 01:34:58 +0300

    Create CheckerForMatrixTest.java

---
+++ test/by/epam/maintasks1/krupin/MainTask/model/logic/CheckerForMatrixTest.java
@@ -0,0 +1,21 @@
+package by.epam.maintasks1.krupin.AdditionalTask.model.logic;
+
+import by.epam.maintasks1.krupin.AdditionalTask.model.data.Matrix;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CheckerForMatrixTest {
+
+    @Test
+    public void checkIsMatrixNullTest(){
+        assertTrue(CheckerForMatrix.checkIsMatrixNull(new Matrix(ParamForTest.matrNull)));
+    }
+
+    @Test
+    public void isSquareMatrixTest(){
+        assertTrue(CheckerForMatrix.isSquareMatrix(new Matrix(ParamForTest.matrSquareSimm)));
+        assertFalse(CheckerForMatrix.isSquareMatrix(new Matrix(ParamForTest.matr)));
+    }
+
+}
