b2da95708551d9fde7b243f2b116c072f7a5f145
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-03-20 01:35:12 +0300

    Create ActionerWithMatrixTest.java

---
+++ test/by/epam/maintasks1/krupin/MainTask/model/logic/ActionerWithMatrixTest.java
@@ -0,0 +1,38 @@
+package by.epam.maintasks1.krupin.AdditionalTask.model.logic;
+
+import by.epam.maintasks1.krupin.AdditionalTask.model.data.Matrix;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ActionerWithMatrixTest {
+
+    @Test
+    public void isMatrixSimetricOnMainDiagonalIfNonSquareMatrTest(){
+        assertFalse(ActionerWithMatrix.isMatrixSimmetricOnMainDiaginal(new Matrix(ParamForTest.matr)));
+    }
+
+    @Test
+    public void isMatrixSimetricOnMainDiagonalTest(){
+        assertTrue(ActionerWithMatrix.isMatrixSimmetricOnMainDiaginal(new Matrix(ParamForTest.matrOneElement)));
+        assertTrue(ActionerWithMatrix.isMatrixSimmetricOnMainDiaginal(new Matrix(ParamForTest.matrSquareSimm)));
+    }
+
+    @Test(expected = Exception.class)
+    public void transposeMatrixIfMatrixIsNullTest() throws Exception {
+        ActionerWithMatrix.transposeMatrix(new Matrix(ParamForTest.matrNull));
+    }
+
+    @Test(expected = Exception.class)
+    public void transposeMatrixIfMatrixIsNonSquareTest() throws Exception {
+        ActionerWithMatrix.transposeMatrix(new Matrix(ParamForTest.matr));
+    }
+
+    @Test
+    public void transposeMatrixTest() throws Exception {
+        double [][] expected = {{2,1,3},{1,1,1},{3,1,2}};
+        assertArrayEquals(expected, ActionerWithMatrix.transposeMatrix(new Matrix(ParamForTest.matrSquareSimm)).getMatr(),
+                ParamForTest.delta);
+    }
+}
