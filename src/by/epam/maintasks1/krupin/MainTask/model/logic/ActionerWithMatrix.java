d06b0d16344683f001c483eb32fba5fe00716e8b
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-03-20 01:33:28 +0300

    Create ActionerWithMatrix.java

---
+++ src/by/epam/maintasks1/krupin/MainTask/model/logic/ActionerWithMatrix.java
@@ -0,0 +1,56 @@
+package by.epam.maintasks1.krupin.AdditionalTask.model.logic;
+
+import by.epam.maintasks1.krupin.AdditionalTask.model.data.Matrix;
+
+import java.sql.SQLOutput;
+
+public class ActionerWithMatrix {
+
+
+    public static boolean isMatrixSimmetricOnMainDiaginal(Matrix matrix) {
+        boolean check = false;
+
+        if (CheckerForMatrix.isSquareMatrix(matrix)) {
+            if (matrix.getMatr().length == 1) {
+                check = true;
+            } else {
+                check = true;
+
+                for (int i = 0; i < matrix.getMatr().length; i++) {
+                    for (int j = i + 1; j < matrix.getMatr().length; j++) {
+                        if (j < matrix.getMatr().length) {
+                            if (matrix.getElements(i, j) != matrix.getElements(j, i)) {
+                                check = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return check;
+    }
+
+    public static Matrix transposeMatrix(Matrix matrix) throws Exception {
+        if (CheckerForMatrix.checkIsMatrixNull(matrix)) {
+           throw  new Exception("Error, matrix is null");
+        }
+
+        if (CheckerForMatrix.isSquareMatrix(matrix) == false) {
+
+           throw  new Exception("Error, matrix is not square");
+        }
+
+        for (int i = 0; i < matrix.getMatr().length; i++) {
+            for (int j = i + 1; j < matrix.getMatr().length; j++) {
+                if (j < matrix.getMatr().length) {
+                    matrix.setElements(i, j, matrix.getElements(i, j) + matrix.getElements(j, i));
+                    matrix.setElements(j, i, matrix.getElements(i, j) - matrix.getElements(j, i));
+                    matrix.setElements(i, j, matrix.getElements(i, j) - matrix.getElements(j, i));
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
