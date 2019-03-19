825090d16c43ac6880052672155d1c6e8a47a5b4
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-03-20 01:34:31 +0300

    Create ParamForTest.java

---
+++ test/by/epam/maintasks1/krupin/MainTask/model/logic/ParamForTest.java
@@ -0,0 +1,11 @@
+package by.epam.maintasks1.krupin.AdditionalTask.model.logic;
+
+public class ParamForTest {
+
+    protected static double delta = 0.0006;
+    protected static double [][] matr = {{2, 3.9, -6},{100, -900.566, 0},{-1000, 1500, -45},{0,0,0}};
+    protected static double [][] matrNull = {};
+    protected static double [][] matrOneElement = {{2}};
+    protected static double [][] matrSquare = {{2, -5, 4},{7, 9, 8},{3, 3, 3}};
+    protected static double [][] matrSquareSimm = {{2, 1, 3},{1, 1, 1},{3, 1, 2}};
+}
