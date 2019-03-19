3fa58d35c9ce199bbfeba715da3ec98d27995640
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-03-20 01:33:00 +0300

    Create ViewMatrix.java

---
+++ src/by/epam/maintasks1/krupin/MainTask/view/ViewMatrix.java
@@ -0,0 +1,10 @@
+package by.epam.maintasks1.krupin.AdditionalTask.view;
+
+import by.epam.maintasks1.krupin.AdditionalTask.model.data.Matrix;
+
+public class ViewMatrix {
+
+    public void showMatrix(Matrix matrix){
+        System.out.println(matrix.toString());
+    }
+}
