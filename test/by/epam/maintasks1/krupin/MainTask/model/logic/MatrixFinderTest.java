7503b0bfcee3553b684f4e1ceb5a9025b975f0a4
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-03-31 21:34:14 +0300

    Update and rename FinderForMatrixTest.java to MatrixFinderTest.java

--- test/by/epam/maintasks1/krupin/MainTask/model/logic/MatrixFinderTest.java
+++ test/by/epam/maintasks1/krupin/MainTask/model/logic/MatrixFinderTest.java
@@ -6,33 +6,33 @@ import by.epam.maintasks1.krupin.AdditionalTask.model.data.Matrix;
 import by.epam.maintasks1.krupin.MainTask.model.logic.ParametrsForActionTest;
 import org.junit.Test;
 
-public class FinderForMatrixTest {
+public class MatrixFinderTest {
 
     @Test(expected = Exception.class)
     public void testFindMaxElementsIfMatrixIsNull() throws Exception {
-        FinderForMatrix.findMaxElement(new Matrix(ParamForTest.matrNull));
+        MatrixFinder.findMaxElement(new Matrix(ParamTest.matrNull));
     }
 
     @Test
     public void testFindMaxElement() throws Exception {
         double expected = 1500.0;
 
-        assertEquals(expected, FinderForMatrix.findMaxElement(new Matrix(ParamForTest.matr)),
-                ParamForTest.delta);
+        assertEquals(expected, MatrixFinder.findMaxElement(new Matrix(ParamTest.matr)),
+                ParamTest.delta);
 
     }
 
     @Test(expected = Exception.class)
     public void testFindMinElementsIfMatrixIsNull() throws Exception {
-        FinderForMatrix.findMinElement(new Matrix(ParamForTest.matrNull));
+        MatrixFinder.findMinElement(new Matrix(ParamTest.matrNull));
     }
 
     @Test
     public void testFindMinElement() throws Exception {
         double expected = -1000;
 
-        assertEquals(expected, FinderForMatrix.findMinElement(new Matrix(ParamForTest.matr)),
-                ParamForTest.delta);
+        assertEquals(expected, MatrixFinder.findMinElement(new Matrix(ParamTest.matr)),
+                ParamTest.delta);
 
     }
 }
