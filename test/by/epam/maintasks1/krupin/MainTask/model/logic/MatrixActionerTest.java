5490a80f9296563dc781464d86dbc36093ac4b3a
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-03-31 21:28:39 +0300

    Update and rename ActionerWithMatrixTest.java to MatrixActionerTest.java

--- test/by/epam/maintasks1/krupin/MainTask/model/logic/MatrixActionerTest.java
+++ test/by/epam/maintasks1/krupin/MainTask/model/logic/MatrixActionerTest.java
@@ -6,32 +6,32 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class ActionerWithMatrixTest {
+public class MatrixActionerTest {
 
     @Test
     public void isMatrixSimetricOnMainDiagonalIfNonSquareMatrTest(){
-        assertFalse(ActionerWithMatrix.isMatrixSimmetricOnMainDiaginal(new Matrix(ParamForTest.matr)));
+        assertFalse(MatrixActioner.isMatrixSimmetricOnMainDiaginal(new Matrix(ParamTest.matr)));
     }
 
     @Test
     public void isMatrixSimetricOnMainDiagonalTest(){
-        assertTrue(ActionerWithMatrix.isMatrixSimmetricOnMainDiaginal(new Matrix(ParamForTest.matrOneElement)));
+        assertTrue(MatrixActioner.isMatrixSimmetricOnMainDiaginal(new Matrix(ParamTest.matrOneElement)));
     }
 
     @Test(expected = Exception.class)
     public void transposeMatrixIfMatrixIsNullTest() throws Exception {
-        ActionerWithMatrix.transposeMatrix(new Matrix(ParamForTest.matrNull));
+        MatrixActioner.transposeMatrix(new Matrix(ParamTest.matrNull));
     }
 
     @Test(expected = Exception.class)
     public void transposeMatrixIfMatrixIsNonSquareTest() throws Exception {
-        ActionerWithMatrix.transposeMatrix(new Matrix(ParamForTest.matr));
+        MatrixActioner.transposeMatrix(new Matrix(ParamTest.matr));
     }
 
     @Test
     public void transposeMatrixTest() throws Exception {
         double [][] expected = {{2,1,3},{1,1,1},{3,1,2}};
-        assertArrayEquals(expected, ActionerWithMatrix.transposeMatrix(new Matrix(ParamForTest.matrSquareSimm)).getMatr(),
-                ParamForTest.delta);
+        assertArrayEquals(expected, MatrixActioner.transposeMatrix(new Matrix(ParamTest.matrSquareSimm)).getMatr(),
+                ParamTest.delta);
     }
 }
