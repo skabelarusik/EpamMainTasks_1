5cc63b9f8650ff3d63e63caff1c6bfdc7f5c246e
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-03-31 21:33:21 +0300

    Update and rename CheckerVectorsTest.java to VectorCheckerTest.java

--- test/by/epam/maintasks1/krupin/MainTask/model/logic/VectorCheckerTest.java
+++ test/by/epam/maintasks1/krupin/MainTask/model/logic/VectorCheckerTest.java
@@ -6,58 +6,58 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
-public class CheckerVectorsTest {
+public class VectorCheckerTest {
 
     @Test(expected = Exception.class)
     public void testCheckNullDoubleVector() throws Exception {
-        CheckerVectors.checkNullVector(new VectorDouble(ParametrsForActionTest.masDoubleNull));
+        VectorChecker.checkNullVector(new VectorDouble(ParametrsActionTest.masDoubleNull));
     }
 
     @Test(expected = Exception.class)
     public void testCheckNullIntVector() throws Exception {
-        CheckerVectors.checkNullVector(new VectorInt(ParametrsForActionTest.masIntNull));
+        VectorChecker.checkNullVector(new VectorInt(ParametrsActionTest.masIntNull));
     }
 
     @Test(expected = Exception.class)
     public void testIsNaturalNumberInVectorInt() throws Exception {
-        CheckerVectors.isNaturalNumber(new VectorInt(ParametrsForActionTest.mas03));
+        VectorChecker.isNaturalNumber(new VectorInt(ParametrsActionTest.mas03));
     }
 
 
     @Test(expected = Exception.class)
     public void testIsSortIncreaseIfNullVector() throws Exception {
-        CheckerVectors.isSortIncrease(new VectorDouble(ParametrsForActionTest.masDoubleNull));
+        VectorChecker.isSortIncrease(new VectorDouble(ParametrsActionTest.masDoubleNull));
 
     }
 
     @Test
     public void testIsSortIncreaseIfSizeIsOne() throws Exception {
         boolean expected = false;
-        assertEquals(expected, CheckerVectors.isSortIncrease(new VectorDouble(ParametrsForActionTest.mas4)));
+        assertEquals(expected, VectorChecker.isSortIncrease(new VectorDouble(ParametrsActionTest.mas4)));
     }
 
     @Test(expected = Exception.class)
     public void testIsSortDecreaseIfNullVector() throws Exception {
-        CheckerVectors.isSortDecrease(new VectorDouble(ParametrsForActionTest.masDoubleNull));
+        VectorChecker.isSortDecrease(new VectorDouble(ParametrsActionTest.masDoubleNull));
 
     }
 
     @Test
     public void testIsSortIncreaseIfSizeMore1() throws Exception {
         boolean expected = true;
-        assertEquals(expected, CheckerVectors.isSortIncrease(new VectorDouble(ParametrsForActionTest.mas3)));
+        assertEquals(expected, VectorChecker.isSortIncrease(new VectorDouble(ParametrsActionTest.mas3)));
     }
 
     @Test
     public void testIsSortDecreaseIfSizeIsOne() throws Exception {
         boolean expected = false;
-        assertEquals(expected, CheckerVectors.isSortDecrease(new VectorDouble(ParametrsForActionTest.mas4)));
+        assertEquals(expected, VectorChecker.isSortDecrease(new VectorDouble(ParametrsActionTest.mas4)));
     }
 
     @Test
     public void testIsSortDecrease() throws Exception {
         boolean expected = true;
-        assertEquals(expected, CheckerVectors.isSortDecrease(new VectorDouble(ParametrsForActionTest.masIsSortDecrise)));
+        assertEquals(expected, VectorChecker.isSortDecrease(new VectorDouble(ParametrsActionTest.masIsSortDecrise)));
     }
 
 }
