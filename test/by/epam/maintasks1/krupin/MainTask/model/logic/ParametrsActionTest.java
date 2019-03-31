07e68f922dee326a073f678b49763d80a433be7c
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-03-31 21:29:09 +0300

    Update and rename ParametrsForActionTest.java to ParametrsActionTest.java

--- test/by/epam/maintasks1/krupin/MainTask/model/logic/ParametrsActionTest.java
+++ test/by/epam/maintasks1/krupin/MainTask/model/logic/ParametrsActionTest.java
@@ -1,6 +1,6 @@
 package by.epam.maintasks1.krupin.MainTask.model.logic;
 
-public class ParametrsForActionTest {
+public class ParametrsActionTest {
     protected static double delta = 0.000006;
 
     public static double [] mas1 = {-5000, 600, 0 , -4500, 7600, 8.0, -900.4, 10_000.76};
